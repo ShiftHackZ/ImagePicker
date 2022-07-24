be7245d248c24b611ad7e913c7d4fceaf982e06a
UmerSarfraz5557 <umersarfraz5557@gmail.com>
2022-07-24 05:15:19 +0500

    multi select from samsung

--- imagepicker/src/main/java/com/shz/imagepicker/imagepicker/GalleryMultiPickerActivity.java
+++ imagepicker/src/main/java/com/shz/imagepicker/imagepicker/GalleryMultiPickerActivity.java
@@ -5,9 +5,11 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.util.Log;
 
@@ -15,6 +17,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 
 public class GalleryMultiPickerActivity extends Activity {
@@ -22,7 +25,7 @@ public class GalleryMultiPickerActivity extends Activity {
     public static ImagePickerCallback mCallback;
 
     private static final int PERMISSION_REQUEST_READ_STORAGE = 54564;
-    private static final int IMAGE_REQUEST_GALLERY = 54565;
+    private static final int IMAGE_REQUEST_GALLERY = 1;
 
     private static final String GALLERY_IMAGE_MIME = "image/jpeg";
 
@@ -48,8 +51,25 @@ public class GalleryMultiPickerActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_REQUEST_GALLERY) {
+
+
+
             if (data != null) {
                 ArrayList<File> files = new ArrayList<>();
+
+
+                if(data.getExtras() != null){
+                    ArrayList<Parcelable> fileUris = data.getExtras().getParcelableArrayList("selectedItems");
+
+                    for(Parcelable uri : fileUris){
+                        if(uri instanceof Uri){
+                            files.add(new File( getRealPathFromURI((Uri) uri) ) );
+                        }
+
+                    }
+
+
+                }
 
                 if (data.getData() != null) {
                     Log.d("MultiPicker", "data: " + data.getData().toString());
@@ -59,7 +79,7 @@ public class GalleryMultiPickerActivity extends Activity {
                         files.add(file);
                     }
                 }
-                
+
                 if (data.getClipData() != null) {
                     ClipData clipData = data.getClipData();
 
@@ -79,6 +99,18 @@ public class GalleryMultiPickerActivity extends Activity {
         finish();
     }
 
+    public String getRealPathFromURI (Uri contentUri) {
+        String path = null;
+        String[] proj = { MediaStore.MediaColumns.DATA };
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+            path = cursor.getString(column_index);
+        }
+        cursor.close();
+        return path;
+    }
+
     private void checkGalleryPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
@@ -93,18 +125,16 @@ public class GalleryMultiPickerActivity extends Activity {
     }
 
     private void startGalleryPicker() {
-//        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-//            galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-//        }
-//        galleryIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, GALLERY_IMAGE_MIME);
-//        startActivityForResult(galleryIntent, IMAGE_REQUEST_GALLERY);
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*"); //allows any image file type. Change * to specific extension to limit it
-//**The following line is the important one!
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        startActivityForResult(Intent.createChooser(intent, "Select Pictures"), IMAGE_REQUEST_GALLERY); //SELECT_PICTURES is simply a global int used to check the calling intent in onActivityResult
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            galleryIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        }
+        galleryIntent.putExtra("multi-pick", true);
+
+
+        //galleryIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, GALLERY_IMAGE_MIME);
+        startActivityForResult(galleryIntent, IMAGE_REQUEST_GALLERY);
+        
     }
 }
