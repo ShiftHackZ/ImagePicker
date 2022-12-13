440e11c129ba85aa0b272f63f00a56c6767e5450
ShiftHackZ <dmitriy@moroz.cc>
2022-12-13 21:11:31 +0200

    Resolve conflicts

--- imagepicker/src/main/java/com/shz/imagepicker/imagepicker/utils/PermissionUtils.kt
+++ imagepicker/src/main/java/com/shz/imagepicker/imagepicker/utils/PermissionUtils.kt
@@ -3,24 +3,30 @@ package com.shz.imagepicker.imagepicker.utils
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.TIRAMISU, lambda = 2)
 fun Activity.checkGalleryNativePermission(request: Int, action: () -> Unit) {
-    if (ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-        ) == PackageManager.PERMISSION_DENIED
-    ) {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
+    if (Build.VERSION.SDK_INT >= 33) action()
+    else {
+        if (ContextCompat.checkSelfPermission(
+                this,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-            ),
-            request,
-        )
-    } else {
-        action()
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                ),
+                request,
+            )
+        } else {
+            action()
+        }
     }
 }
 
