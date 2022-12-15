7f146c309ce50e22c12cfdfdac5bff88c728c819
ShiftHackZ <dmitriy@moroz.cc>
2022-12-15 20:16:15 +0200

    Custom multi-selection gallery stability fixes

--- app/src/main/java/com/shz/imagepicker/imagepickerapp/MainActivity.kt
+++ app/src/main/java/com/shz/imagepicker/imagepickerapp/MainActivity.kt
@@ -2,18 +2,16 @@ package com.shz.imagepicker.imagepickerapp
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bumptech.glide.Glide
 import com.shz.imagepicker.imagepicker.ImagePicker
 import com.shz.imagepicker.imagepicker.ImagePickerCallback
-import com.shz.imagepicker.imagepicker.ImagePickerLoadDelegate
 import com.shz.imagepicker.imagepicker.model.GalleryPicker
 import com.shz.imagepicker.imagepicker.model.PickedResult
 import com.shz.imagepicker.imagepickerapp.databinding.ActivityMainBinding
-import java.io.File
 
 class MainActivity : AppCompatActivity(), ImagePickerCallback {
 
@@ -26,64 +24,8 @@ class MainActivity : AppCompatActivity(), ImagePickerCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        adapter = ImagesDemoAdapter()
-
-        binding.rv.adapter = adapter
-        binding.rv.layoutManager = GridLayoutManager(this, 2)
-
-        binding.btnCamera.setOnClickListener {
-            imagePicker
-                .autoRotate(binding.switchAutoRotate.isChecked)
-                .useCamera()
-                .build()
-                .launch(this)
-        }
-        binding.btnGallery.setOnClickListener {
-            imagePicker
-                .autoRotate(binding.switchAutoRotate.isChecked)
-                .useGallery()
-//                .multipleSelection(true)
-                .build()
-                .launch(this)
-        }
-        binding.btnGalCustomSingle.setOnClickListener {
-            imagePicker
-                .autoRotate(binding.switchAutoRotate.isChecked)
-                .useGallery()
-                .galleryPicker(GalleryPicker.CUSTOM)
-                .loadDelegate { imageView, file ->
-                    Glide.with(this)
-                        .load(file)
-                        .into(imageView)
-                }
-                .build()
-                .launch(this)
-        }
-        binding.btnGalCustomMultiple.setOnClickListener {
-            imagePicker
-                .autoRotate(binding.switchAutoRotate.isChecked)
-                .useGallery()
-                .multipleSelection()
-                .galleryPicker(GalleryPicker.CUSTOM)
-                .loadDelegate { imageView, file ->
-                    Glide.with(this)
-                        .load(file)
-                        .into(imageView)
-                }
-                .build()
-                .launch(this)
-        }
-        binding.btnDialog.setOnClickListener {
-            imagePicker
-                .autoRotate(binding.switchAutoRotate.isChecked)
-                .useGallery()
-                .useCamera()
-                .multipleSelection()
-                .galleryPicker(GalleryPicker.NATIVE)
-                .build()
-                .launch(this)
-        }
+        initializeButtons()
+        initializeImagesList()
     }
 
     override fun onImagePickerResult(result: PickedResult) {
@@ -94,6 +36,11 @@ class MainActivity : AppCompatActivity(), ImagePickerCallback {
             }
             is PickedResult.Error -> {
                 result.throwable.printStackTrace()
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.action_error)
+                    .setMessage(result.throwable.message.toString())
+                    .setPositiveButton(R.string.action_ok) { d, _ -> d.dismiss() }
+                    .show()
             }
             is PickedResult.Multiple -> {
                 adapter.submitList(result.images)
@@ -102,5 +49,53 @@ class MainActivity : AppCompatActivity(), ImagePickerCallback {
                 adapter.submitList(listOf(result.image))
             }
         }
+    }
+
+    private fun initializeButtons() = with(binding) {
+        btnCamera.setOnClickListener {
+            imagePicker
+                .autoRotate(binding.switchAutoRotate.isChecked)
+                .useCamera()
+                .build()
+                .launch(this@MainActivity)
+        }
+        btnGallery.setOnClickListener {
+            imagePicker
+                .autoRotate(binding.switchAutoRotate.isChecked)
+                .useGallery()
+                .multipleSelection(binding.switchMultipleSelection.isChecked)
+                .build()
+                .launch(this@MainActivity)
+        }
+        btnGalCustomSingle.setOnClickListener {
+            imagePicker
+                .autoRotate(binding.switchAutoRotate.isChecked)
+                .useGallery()
+                .multipleSelection(binding.switchMultipleSelection.isChecked)
+                .galleryPicker(GalleryPicker.CUSTOM)
+                .loadDelegate { imageView, file ->
+                    Glide.with(imageView)
+                        .load(file)
+                        .into(imageView)
+                }
+                .build()
+                .launch(this@MainActivity)
+        }
+        btnDialog.setOnClickListener {
+            imagePicker
+                .autoRotate(binding.switchAutoRotate.isChecked)
+                .useGallery()
+                .useCamera()
+                .multipleSelection(binding.switchMultipleSelection.isChecked)
+                .galleryPicker(GalleryPicker.NATIVE)
+                .build()
+                .launch(this@MainActivity)
+        }
+    }
+
+    private fun initializeImagesList() = with(binding) {
+        adapter = ImagesDemoAdapter()
+        recyclerImages.adapter = adapter
+        recyclerImages.layoutManager = GridLayoutManager(this@MainActivity, 2)
     }
 }
